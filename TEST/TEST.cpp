9932630e9fa22dc38f74ece908ed1a31b2d29456
deprphyc02qsad <105680034+anonimchqwe@users.noreply.github.com>
2023-08-31 07:10:25 +0300

    Add files via upload

---
+++ TEST/TEST.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	const int momerbati = 1531212;//NUMERO UNO
+	cout << momerbati;//NUMERO DOS
+	double maimai = 23.59;
+	cout << maimai;
+	int momerbati = 2388;
+	cout << "ja loh" << maimai << momerbati;
+	string jopa = "asjdhasdhas";
+	cout << jopa;
+
+	int x;
+	cout << "TYPE ANYTHING!";
+	cin >> x;
+	cout << "YA NAMBA IS\t" << x;
+	int x, y;
+	int sum;
+	cout << "Type a number: ";
+	cin >> x;
+	cout << "Type another number: ";
+	cin >> y;
+	sum = x + y;
+	cout << "Sum is: " << sum;
+
+	int myNum = 5;               // Integer (whole number)
+	float myFloatNum = 5.99;     // Floating point number
+	double myDoubleNum = 9.98;   // Floating point number
+	char myLetter = 'D';         // Character
+	bool myBoolean = true;       // Boolean
+	string myText = "Hello";     // String
+
+	float f1 = 35e3;
+	double d1 = 12E4;
+	cout << f1;
+	cout << d1;
+
+	bool isCodingFun = true;
+	bool isFishTasty = false;
+	cout << isCodingFun;  // Outputs 1 (true)
+	cout << isFishTasty;  // Outputs 0 (false)
+
+	char a = 65, b = 66, c = 67;
+	cout << a;
+	cout << b;
+	cout << c;
+
+	char myGrade = 'B';
+	cout << myGrade;
+
+	string greeting = "Hello";
+	cout << greeting;
+
+
+	// Include the string library
+#include <string>
+
+// Create a string variable
+	string greeting = "Hello";
+
+	// Output string value
+	cout << greeting;
+
+	
+}
